132b3697ae34e1cc5d1f0ef247d73b24cb40725b
Aditya Kale <kalepunam83@gmail.com>
2025-07-28 21:16:00 +0530

    adding maxConsecutiveOnes

---
+++ 485maxConsecOne.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0, maxone = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                maxone = max(maxone, count);
+                count = 0;
+            }
+
+            maxone = max(maxone, count);
+        }
+
+        return maxone;
+    }
+};
